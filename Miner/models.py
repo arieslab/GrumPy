6917fd5a38d18c861fcf82203164dbc0f7a10f55
Joselito Jr <motajoselito@gmail.com>
2020-11-08 07:27:03 -0300

    Adding the Token models and migrations files

--- Miner/models.py
+++ Miner/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Token(models.Model):
+    tokenname = models.CharField('Name', max_length=100)
+    token = models.CharField('Token', max_length=100)
+
+    def __str__(self):
+        return self.tokenname
