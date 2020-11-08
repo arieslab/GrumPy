56283eb772abeeb88891380437c35762fea53257
railana <railanassantana@gmail.com>
2020-11-08 13:40:22 -0300

    Create Keys Screen

---
+++ Miner/static/js/script.js
@@ -0,0 +1,5 @@
+function addKey() {
+    nome_key = document.getElementById("field_nameKey").value;
+    key =  document.getElementById("field_key").value;
+    alert("Nome: " + nome_key + " - Chave: " + key);
+}
