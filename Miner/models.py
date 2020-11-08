b6b9ca166ed1518d4830527653ae41699581fb5b
Joselito Jr <motajoselito@gmail.com>
2020-11-08 07:44:11 -0300

    Adding models and migrations to Miner

--- Miner/models.py
+++ Miner/models.py
@@ -7,3 +7,16 @@ class Token(models.Model):
 
     def __str__(self):
         return self.tokenname
+
+
+class Miner(models.Model):
+    minername = models.CharField('Name', max_length=100)  # Miner Name
+    repoamount = models.IntegerField('Repo amount')  # Repo amount of listed repo
+    minedamount = models.IntegerField('Mined Repo Amount')  # Mined amount
+    minerstatus = models.CharField('Status', max_length=100)  # Miner task status
+    minertaskid = models.CharField('Task id', max_length=100)  # Asynchronous task id
+    tokenassociated = models.ForeignKey(Token, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.minername
