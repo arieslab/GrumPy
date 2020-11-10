b45a1570e689dd4d9c66c7371502772b5755e83f
Joselito Jr <motajoselito@gmail.com>
2020-11-10 17:06:54 -0300

    Adding event mining method and refactoring init method

--- Miner/Activity_performance/MinersClass.py
+++ Miner/Activity_performance/MinersClass.py
@@ -2,10 +2,45 @@ import requests
 import sys
 from requests import exceptions
 from github import Github, GithubException
+from Miner.Activity_performance import RequestVerificationClass
+from Miner.Issues_Persistence.PersistencePattern import PersistencePattern
 
 class MinersClass():
-    def __init__(self, issue, authentication):
+    def __init__(self, issue, authentication, time_to_wait, set_num_requests):
         self.issue = issue
         self.authentication = authentication
+        self.time_to_wait = time_to_wait
+        self.num_requests = set_num_requests
 
-    
+    def event_mining(self, issue, authentication):
+        issue_events_list = []
+        RequestVerificationClass(self.authentication, self.time_to_wait, self.num_requests)
+
+        try:
+
+            for event in self.issue.get_events():
+                RequestVerificationClass(self.authentication, self.time_to_wait, self.num_requests)
+                e = ''
+                pattern = PersistencePattern()
+
+                if(event.actor is None):
+                    if (event.label is None):
+                        event_formatted = pattern.eventPattern([issue.number, '-', event.created_at, event.event, '-'])
+                    else:
+                        event_formatted = pattern.eventPattern([issue.number, '-', event.created_at, event.event, event.label.name])
+
+                else:
+                    if(event.label is None):
+                        event_formatted = pattern.eventPattern([issue.number, event.actor.login, event.created_at, event.event, '-'])
+                    else:
+                        event_formatted = pattern.eventPattern([issue.number, event.actor.login, event.created_at, event.event, event.label.name])
+                issue_events_list.append(event_formatted)
+        except requests.exceptions.ReadTimeout as aes:
+            raise SystemError('ReadTimeout error in event mining')
+        except requests.exceptions.ConnectionError as aes:
+            raise SystemError('Connection error in event mining')
+        except GithubException as d:
+            if (d.status == 403):
+                raise SystemError('Request limit achieved in event mining ')
+
+        return issue_events_list
