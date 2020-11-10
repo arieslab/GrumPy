1e1474763a457ca2735fd95e164c3940378d3cf1
Joselito Jr <motajoselito@gmail.com>
2020-11-10 14:20:41 -0300

    Adding class and open connections method

--- Miner/Issues_Persistence/Connections.py
+++ Miner/Issues_Persistence/Connections.py
@@ -4,4 +4,17 @@ PLACE = 'localhost'
 PORT = 27017
 DB_NAME = 'GithubIssuesDB'
 
+class Connections():
+    def __init__(self):
+        self.client, self.issues_db = self.openConnectionToDB()
+
+    def openConnectionToDB(self):
+        mongodb_client = MongoClient(PLACE,
+                                     PORT)
+
+        issues_database = mongodb_client[DB_NAME]
+
+        return mongodb_client, issues_database
+
+
 
