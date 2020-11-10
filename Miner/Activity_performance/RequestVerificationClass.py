32e293c22dd31e628975ca1c194e6eba4357f6ef
Joselito Jr <motajoselito@gmail.com>
2020-11-10 16:21:32 -0300

    Adding method to wait request refreshing and refactoring the init and verify_limit methods

--- Miner/Activity_performance/RequestVerificationClass.py
+++ Miner/Activity_performance/RequestVerificationClass.py
@@ -1,20 +1,26 @@
+from datetime import time
 import requests
-from requests import exceptions
-from github import Github, GithubException
 
 
 class RequestVerificationClass():
-    def __init__(self, authentication, limit):
+    def __init__(self, authentication, limit, time_to_wait):
         self.authentication = authentication
         self.limit = limit
+        self.time_to_wait = time_to_wait
+        self.requests = self.verify_rate_limit()
+
+        if (self.requests < self.time_to_wait):
+            self.wait_until()
 
     def verify_rate_limit(self):
         try:
             request_amount = int(self.authentication.get_rate_limit().core.remaining)
 
-            if (request_amount < self.limit):
-                return 'Wait '
-            else:
-                return 'Free to go'
+            return request_amount
         except requests.exceptions.ConnectionError:
             raise SystemError('ConnectionError')
+
+    def wait_until(self):
+        while (self.requests < self.limit):
+            time.sleep(self.time_to_wait)
+            self.requests = self.verify_rate_limit()
