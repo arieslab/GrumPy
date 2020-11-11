ace73111e5bc1f235e1b823b64999ce59171b584
Joselito Jr <motajoselito@gmail.com>
2020-11-11 20:06:48 -0300

    Fixing issues and adding methods to mine the issue, get issue and mining the repo and issues labels

--- Miner/Activity_performance/MinersClass.py
+++ Miner/Activity_performance/MinersClass.py
@@ -2,43 +2,42 @@ import requests
 import sys
 from requests import exceptions
 from github import Github, GithubException
-from Miner.Activity_performance import RequestVerificationClass
+from Miner.Activity_performance.RequestVerificationClass import VerificationClass
 from Miner.Issues_Persistence.PersistencePattern import PersistencePattern
 
 
-class MinersClass:
-    def __init__(self, issue, authentication, time_to_wait, set_num_requests, repository):
-        self.issue = issue
-        self.repository = repository
+class MinerClass:
+    def __init__(self, authentication, time_to_wait, set_num_requests, repo):
+        self.repository = authentication.get_repo(str(repo))
         self.authentication = authentication
         self.time_to_wait = time_to_wait
         self.num_requests = set_num_requests
 
     def event_mining(self, issue):
         issue_events_list = []
-        RequestVerificationClass(self.authentication, self.time_to_wait, self.num_requests)
+        VerificationClass(self.authentication, self.time_to_wait, self.num_requests)
 
         try:
 
-            for event in self.issue.get_events():
-                RequestVerificationClass(self.authentication, self.time_to_wait, self.num_requests)
+            for event in issue.get_events():
+                VerificationClass(self.authentication, self.time_to_wait, self.num_requests)
                 e = ''
                 pattern = PersistencePattern()
 
                 if (event.actor is None):
                     if (event.label is None):
-                        event_formatted = pattern.eventPattern([issue.number, '-', event.created_at, event.event, '-'])
+                        event_formatted = pattern.eventPattern(issue.number, '-', event.created_at, event.event, '-')
                     else:
                         event_formatted = pattern.eventPattern(
-                            [issue.number, '-', event.created_at, event.event, event.label.name])
+                            issue.number, '-', event.created_at, event.event, event.label.name)
 
                 else:
                     if (event.label is None):
                         event_formatted = pattern.eventPattern(
-                            [issue.number, event.actor.login, event.created_at, event.event, '-'])
+                            issue.number, event.actor.login, event.created_at, event.event, '-')
                     else:
                         event_formatted = pattern.eventPattern(
-                            [issue.number, event.actor.login, event.created_at, event.event, event.label.name])
+                            issue.number, event.actor.login, event.created_at, event.event, event.label.name)
                 issue_events_list.append(event_formatted)
         except requests.exceptions.ReadTimeout as aes:
             raise SystemError('ReadTimeout error in event mining')
@@ -50,26 +49,25 @@ class MinersClass:
 
         return issue_events_list
 
-    def comments_mining(self):
+    def comments_mining(self, issue):
         issue_comments_list = []
-        RequestVerificationClass(self.authentication, self.time_to_wait, self.num_requests)
+        VerificationClass(self.authentication, self.time_to_wait, self.num_requests)
 
         try:
-            for comment in self.issue.get_comments():
-                RequestVerificationClass(self.authentication, self.time_to_wait, self.num_requests)
+            for comment in issue.get_comments():
+                VerificationClass(self.authentication, self.time_to_wait, self.num_requests)
                 pattern = PersistencePattern()
 
-                RequestVerificationClass(self.authentication, self.time_to_wait, self.num_requests)
+                VerificationClass(self.authentication, self.time_to_wait, self.num_requests)
                 reactions = self.reactions_mining(comment)
 
                 if (comment.user is None):
-                    comment_formatted = pattern.CommentsPattern(['-', comment.created_at, comment.body, reactions])
+                    comment_formatted = pattern.CommentsPattern('-', comment.created_at, comment.body, reactions)
                 else:
                     comment_formatted = pattern.CommentsPattern(
-                        [comment.user.login, comment.created_at, comment.body, reactions])
+                        comment.user.login, comment.created_at, comment.body, reactions)
 
                 issue_comments_list.append(comment_formatted)
-
 
         except requests.exceptions.ReadTimeout as aes:
             raise SystemError('ReadTimeout error in event mining')
@@ -81,7 +79,7 @@ class MinersClass:
 
     def reactions_mining(self, element):
         issue_reactions_list = []
-        RequestVerificationClass(self.authentication, self.time_to_wait, self.num_requests)
+        VerificationClass(self.authentication, self.time_to_wait, self.num_requests)
         pattern = PersistencePattern()
         reactions_list = {'+1': 0,
                           'heart': 0,
@@ -94,7 +92,7 @@ class MinersClass:
 
         try:
             for reaction in element.get_reactions():
-                RequestVerificationClass(self.authentication, self.time_to_wait, self.num_requests)
+                VerificationClass(self.authentication, self.time_to_wait, self.num_requests)
                 TheReaction = reaction.content()
 
                 reactions_list[str(TheReaction)] += 1
@@ -107,24 +105,34 @@ class MinersClass:
             if (d.status == 403):
                 raise SystemError('Request limit achieved in event mining ')
 
-        return pattern.ReactionPattern([reactions_list['+1'],
-                                        reactions_list['heart'],
-                                        reactions_list['hooray'],
-                                        reactions_list['confused'],
-                                        reactions_list['-1'],
-                                        reactions_list['laugh'],
-                                        reactions_list['rocket'],
-                                        reactions_list['eyes']
-                                        ])
+        return pattern.ReactionPattern(reactions_list)
 
-    def repo_labels_mining(self, component):
-        issue_labels_list = []
-        RequestVerificationClass(self.authentication, self.time_to_wait, self.num_requests)
+    def repo_labels_mining(self):
+        repo_labels_list = []
+        VerificationClass(self.authentication, self.time_to_wait, self.num_requests)
         pattern = PersistencePattern()
 
         try:
-            for label in component.get_labels():
-                issue_labels_list.append(label)
+            for label in self.repository.get_labels():
+                repo_labels_list.append(label.name)
+        except requests.exceptions.ReadTimeout as aes:
+            raise SystemError('ReadTimeout error in event mining')
+        except requests.exceptions.ConnectionError as aes:
+            raise SystemError('Connection error in event mining')
+        except GithubException as d:
+            if (d.status == 403):
+                raise SystemError('Request limit achieved in event mining ')
+
+        return pattern.LabelsPattern(repo_labels_list)
+
+    def issue_labels_mining(self, issue):
+        issue_labels_list = []
+        VerificationClass(self.authentication, self.time_to_wait, self.num_requests)
+        pattern = PersistencePattern()
+
+        try:
+            for label in issue.get_labels():
+                issue_labels_list.append(label.name)
         except requests.exceptions.ReadTimeout as aes:
             raise SystemError('ReadTimeout error in event mining')
         except requests.exceptions.ConnectionError as aes:
@@ -135,13 +143,43 @@ class MinersClass:
 
         return pattern.LabelsPattern(issue_labels_list)
 
-    def getLastIssue(self, repo):
-        try:
-            RequestVerificationClass(self.authentication, self.time_to_wait, self.num_requests)
-            repository = self.authentication.get_repo(repo)
 
-            for issue in repository.get_issues(state='all'):
+    def getLastIssue(self):
+        try:
+            for issue in self.repository.get_issues(state='all'):
                 return int(issue.number)
         except GithubException as d:
-            if(d.status == 404):
+            if (d.status == 404):
                 raise SystemError('Error 404')
+
+    def getIssue(self, number):
+        try:
+            VerificationClass(self.authentication, self.time_to_wait, self.num_requests)
+            issue = self.repository.get_issue(number)
+            return issue
+        except GithubException as f:
+            if (f.status == 404):
+                return None
+        except AttributeError as a:
+            return None
+
+    def issue_mining(self, issue):
+        pattern = PersistencePattern()
+
+        issue_event = self.event_mining(issue)
+        issue_comments = self.comments_mining(issue)
+        issue_reactions = self.reactions_mining(issue)
+        issue_labels = self.issue_labels_mining(issue)
+        repository_labels = self.repo_labels_mining()
+        return pattern.issuePattern(self.repository.name,
+                                     issue.number,
+                                     issue.user.login,
+                                     issue.created_at,
+                                     issue.state,
+                                     issue.title,
+                                     issue.body,
+                                     repository_labels,
+                                     issue_reactions,
+                                     issue_event,
+                                     issue_comments,
+                                     issue_labels)
