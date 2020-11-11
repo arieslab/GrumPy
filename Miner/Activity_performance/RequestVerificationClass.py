e6ca8cd8081908ba2c24394e52ce613784c6c30d
Joselito Jr <motajoselito@gmail.com>
2020-11-11 20:07:56 -0300

    Refactoring code to fix issues in request verification module

--- Miner/Activity_performance/RequestVerificationClass.py
+++ Miner/Activity_performance/RequestVerificationClass.py
@@ -1,26 +1,30 @@
-from datetime import time
+import time
 import requests
+import github
+from datetime import time
+from requests import exceptions
+from github import Github, GithubException
 
 
-class RequestVerificationClass:
+class VerificationClass:
     def __init__(self, authentication, limit, time_to_wait):
         self.authentication = authentication
         self.limit = limit
         self.time_to_wait = time_to_wait
-        self.requests = self.verify_rate_limit()
+        self.Requests_Amount = 30
 
-        if (self.requests < self.time_to_wait):
+        if (self.Requests_Amount < self.time_to_wait):
             self.wait_until()
 
     def verify_rate_limit(self):
         try:
-            request_amount = int(self.authentication.get_rate_limit().core.remaining)
+            self.Requests_Amount = int(self.authentication.get_rate_limit().core.remaining)
 
-            return request_amount
+            return self.Requests_Amount
         except requests.exceptions.ConnectionError:
             raise SystemError('ConnectionError')
 
     def wait_until(self):
-        while (self.requests < self.limit):
+        while (self.Requests_Amount < self.limit):
             time.sleep(self.time_to_wait)
-            self.requests = self.verify_rate_limit()
+            self.Requests_Amount = self.verify_rate_limit()
