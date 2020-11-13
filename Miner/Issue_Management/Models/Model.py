f22a1297bbfcc46181dcf904126249d343d20b94
Joselito Jr <motajoselito@gmail.com>
2020-11-13 09:49:54 -0300

    Adding the retrive info of open, closed and all issues in one method

--- Miner/Issue_Management/Models/Model.py
+++ Miner/Issue_Management/Models/Model.py
@@ -7,17 +7,22 @@ class RepositoryClass:
         self.repository_name = name
         self.amount_closed_issues = self.amount_open_issues = self.amount_of_issues = 0
 
-    def getAmountOfIssues(self):
+    def getAmountOfIssues(self, state):
         db_Connection = Connections()
 
         db_Connection.openConnectionToDB()
 
-        repository = db_Connection[self.repository_name]
-
-        self.amount_of_issues = repository.count()
-
-        print(self.amount_of_issues)
+        if(state == 'all'):
+            self.amount_of_issues = db_Connection.getAmountInCollection(self.repository_name)
+            return self.amount_of_issues
+        elif(state == 'open'):
+            self.amount_open_issues = db_Connection. getIssuesByStatus(self.repository_name, state).count()
+            return self.amount_open_issues
+        elif(state == 'closed'):
+            self.amount_closed_issues = db_Connection.getIssuesByStatus(self.repository_name, state).count()
+            return self.amount_closed_issues
 
         db_Connection.closeConnectionToDB()
+
 
 
