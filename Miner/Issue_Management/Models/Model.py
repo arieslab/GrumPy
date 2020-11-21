e7db5592f7aeacefb4cfc3bc1c73ee7015fb51c9
Joselito Jr <motajoselito@gmail.com>
2020-11-21 13:03:59 -0300

    Adding method to get all issues ids of a collections in database

--- Miner/Issue_Management/Models/Model.py
+++ Miner/Issue_Management/Models/Model.py
@@ -9,23 +9,31 @@ class RepositoryClass:
         self.getAmountOfIssues('open')
         self.getAmountOfIssues('closed')
         self.getAmountOfIssues('all')
+        self.repository_name_url = name.replace('/', '%2F')
 
     def getAmountOfIssues(self, state):
         db_Connection = Connections()
 
         db_Connection.openConnectionToDB()
 
-        if(state == 'all'):
+        if (state == 'all'):
             self.amount_of_issues = db_Connection.getAmountInCollection(self.repository_name)
             return self.amount_of_issues
-        elif(state == 'open'):
-            self.amount_open_issues = db_Connection. getIssuesByStatus(self.repository_name, state).count()
+        elif (state == 'open'):
+            self.amount_open_issues = db_Connection.getIssuesByStatus(self.repository_name, state).count()
             return self.amount_open_issues
-        elif(state == 'closed'):
+        elif (state == 'closed'):
             self.amount_closed_issues = db_Connection.getIssuesByStatus(self.repository_name, state).count()
             return self.amount_closed_issues
 
         db_Connection.closeConnectionToDB()
 
 
-
+class IssueIndex:
+    def __init__(self, repoName, id, status, comments, reactions, events):
+        self.id = id
+        self.repository_name = repoName
+        self.status = status
+        self.comments = comments
+        self.reactions = reactions
+        self.events = events
